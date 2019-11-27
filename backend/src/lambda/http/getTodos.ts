a43bf7df56b2ec7c65f0005e3e8f7c6c7331b25e
Maxim Kugatov <maximkugatov@gmail.com>
2019-11-28 01:46:18 +0200

    feat: getTodoBuUserId works

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,10 +1,20 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getTodoByUserId } from '../../businessLogic/todos'
 
-// // TODO: per user !!!
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
+  console.log(event)
+  const todos = await getTodoByUserId("kugmax") //TODO: need return	
 
-//   // TODO: Get all TODO items for a current user
-// }
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: todos
+    })
+  }
+}
